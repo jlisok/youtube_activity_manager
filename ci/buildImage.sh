2e0498c5d7df9d75f9252f7f0a929cbce009f2e5
Justyna <justyna.lisok@gmail.com>
2020-10-05 14:06:32 +0200

    [fix] AWS flow

--- ci/buildImage.sh
+++ ci/buildImage.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-# IMAGE_VERSION will be passed through environment variables
-
-IMAGE_VERSION_STRING=${IMAGE_VERSION:-"0.0.1-SNAPSHOT"}
-IMAGE_NAME="com.jlisok/youtube_activity_manager:${IMAGE_VERSION_STRING}"
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-"${SCRIPT_DIR}"/../gradlew bootBuildImage --imageName "${IMAGE_NAME}"
+# IMAGE_VERSION will be passed through environment variables
+IMAGE_VERSION_STRING=${IMAGE_VERSION:-"0.0.1-SNAPSHOT"}
+IMAGE_TAG="com.jlisok/youtube_activity_manager:${IMAGE_VERSION_STRING}"
+ECR_IMAGE_TAG="955603615851.dkr.ecr.eu-central-1.amazonaws.com/${IMAGE_TAG}"
+
+"${SCRIPT_DIR}"/../gradlew bootBuildImage --imageName "${IMAGE_TAG}"
+docker tag "${IMAGE_TAG}" "${ECR_IMAGE_TAG}"
+docker push "${ECR_IMAGE_TAG}"
+
+
 
