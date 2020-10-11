e0d7723e941fe82f7b34ffacef2ad48bfe2259d1
Justyna <justyna.lisok@gmail.com>
2020-10-12 01:50:26 +0200

    [fix] restoring changes on changelogs from commit 13

--- src/main/resources/db/changelog/changelog-0.0.3.sql
+++ src/main/resources/db/changelog/changelog-0.0.3.sql
@@ -1,42 +1,58 @@
 --liquibase formatted sql
 
 --changeset jlisok:1
-CREATE TABLE public.channels (
+CREATE TYPE public.rating AS ENUM (
+    'LIKE',
+    'DISLIKE'
+);
+
+CREATE TABLE public.videos (
     id uuid PRIMARY KEY NOT NULL,
-    youtube_channel_id text NOT NULL,
     title text NOT NULL,
-    published_at timestamp without time zone NOT NULL,
-    language text,
-    country text,
-    owner text,
-    number_of_views bigint NOT NULL,
-    number_of_subscribers bigint NOT NULL,
-    number_of_videos integer NOT NULL,
-    created_at timestamp without time zone DEFAULT now() NOT NULL,
-    modified_at timestamp without time zone DEFAULT now() NOT NULL
-);
-
-CREATE TABLE public.users_channels (
-    id uuid PRIMARY KEY NOT NULL,
     channel_id uuid NOT NULL,
-    user_id uuid NOT NULL,
+    duration interval NOT NULL,
+    published_at timestamp without time zone NOT NULL,
+    hashtag text[],
+    uri text[],
     created_at timestamp without time zone DEFAULT now() NOT NULL,
     modified_at timestamp without time zone DEFAULT now() NOT NULL
 );
 
-ALTER TABLE public.users_channels
-    ADD CONSTRAINT channel_id_fkey_channels FOREIGN KEY (channel_id) REFERENCES public.channels(id)
-    ON UPDATE CASCADE;
+CREATE TABLE public.users_videos (
+    id uuid PRIMARY KEY NOT NULL,
+    user_id uuid NOT NULL,
+    video_id uuid NOT NULL,
+    rating public.rating NOT NULL,
+    created_at timestamp without time zone DEFAULT now() NOT NULL,
+    modified_at timestamp without time zone DEFAULT now() NOT NULL
+);
 
-ALTER TABLE public.users_channels
-    ADD CONSTRAINT user_id_fkey_users FOREIGN KEY (user_id) REFERENCES public.users(id)
+ALTER TABLE public.users_videos
+    ADD CONSTRAINT video_id_fkey_users FOREIGN KEY (user_id) REFERENCES public.users(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE;
+
+ALTER TABLE public.users_videos
+    ADD CONSTRAINT video_id_fkey_videos FOREIGN KEY (video_id) REFERENCES public.videos(id)
     ON UPDATE CASCADE
     ON DELETE CASCADE;
 
 
---rollback ALTER TABLE public.users_channels DROP CONSTRAINT user_id_fkey_users;
---rollback ALTER TABLE public.users_channels DROP CONSTRAINT channel_id_fkey_channels;
---rollback DROP TABLE public.users_channels CASCADE;
---rollback DROP TABLE public.channels CASCADE;
+--rollback DROP TABLE public.videos CASCADE;
+--rollback DROP TABLE public.users_videos CASCADE;
+--rollback DROP TYPE public.rating;
 
 
+--changeset jlisok:2
+ALTER TABLE public.videos
+    ADD youtube_id text NOT NULL;
+
+
+--rollback ALTER TABLE public.videos DROP COLUMN youtube_id;
+
+--changeset jlisok:3
+ALTER TABLE public.videos
+    ALTER COLUMN channel_id TYPE text,
+    ALTER COLUMN channel_id SET NOT NULL;
+
+--rollback ALTER TABLE public.videos ALTER COLUMN channel_id TYPE uuid, ALTER COLUMN channel_id SET NOT NULL;
